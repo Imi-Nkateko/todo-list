e5fa15e0a1ce623584ee8d8349315bdfff07484b
Nkateko Shikuhele <[shikuhelenkateko@gmail.com]>
2024-10-01 18:46:36 +0200

    create a demo dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import "./style.css";
-import { greeting } from "./greeting.js";
+
+
+
+
+    const taskBtn = document.querySelector("#addTask");
+
+    taskBtn.addEventListener("click", () => {
+        todoForm();
+    })
+
+
+function todoForm() {
+    const formContainer = document.querySelector(".todo-container");
+    const formdiv = document.createElement("div");
+    formdiv.classList.add("form-div");
+    formdiv.innerHTML = `
+        <form>
+        <h1>hello</h1>
+        </form>`;
+    formContainer.appendChild(formdiv)    
+}
 
